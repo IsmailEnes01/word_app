1df4a16b54a1ad0a1b3620f2362d27a39a7c1e8f
IsmailEnes01 <115553409+IsmailEnes01@users.noreply.github.com>
2026-08-31 16:05:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lugat-v4";
+const CACHE="lugat-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png","./icon-maskable-512.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
